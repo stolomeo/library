176c526d93c1a8c8838adcdcd6822df626f12a93
stolomeo <tolomeosebastian@gmail.com>
2022-03-12 14:14:19 -0500

    Inital commit

---
+++ script.js
@@ -0,0 +1,21 @@
+/*
+function addBooktoLibrary
+push title, author, pages, and read value into myLibrary[];
+
+function create card
+take each array in myLibrary and create card
+
+submit button
+create card, clear form, prevent refresh
+*/
+
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBooktoLibrary() {}
