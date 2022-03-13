468eeefa290f8bd6949e7e103073c4ba96c002b1
stolomeo <tolomeosebastian@gmail.com>
2022-03-13 14:56:12 -0400

    Add basic remove book button to each card

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ take each array in myLibrary and create card
 submit button
 create card, clear form, prevent refresh
 */
-
+const library = document.querySelector(".library-cards");
+const card = document.querySelector(".card");
 let myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -18,11 +19,10 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
-Book.prototype.retrieveValues = function () {};
-
 function addBooktoLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  displayBook(newBook);
 }
 
 function formSubmit(event) {
@@ -39,3 +39,38 @@ function formClear() {
   (title.value = ""), (author.value = ""), (pages.value = "");
   read.checked = false;
 }
+
+function displayBook(book) {
+  const div = document.createElement("div");
+  div.classList.add("card");
+  library.appendChild(div);
+
+  const title = document.createElement("h3");
+  title.innerText = `${book.title}`;
+  div.appendChild(title);
+
+  const author = document.createElement("p");
+  author.innerText = `Author: ${book.author}`;
+  div.appendChild(author);
+
+  const pages = document.createElement("p");
+  pages.innerText = `Pages: ${book.pages}`;
+  div.appendChild(pages);
+
+  const read = document.createElement("p");
+  if (book.read) {
+    read.innerText = "Read";
+  } else {
+    read.innerText = "Unread";
+  }
+  div.appendChild(read);
+
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "Remove Book";
+  deleteBtn.addEventListener("click", (e) => {
+    library.removeChild(div);
+  });
+  div.appendChild(deleteBtn);
+}
+
+addBooktoLibrary("Harry Potter", "JK Rowling", "935", true);
